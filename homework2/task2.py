4dc7189ce55c1e0136ccda8982ce62113215397e
TayaL <tayalyapina555@gmail.com>
2023-08-16 10:26:29 +0300

    File with task2 in folder homework2 modified

--- homework2/task2.py
+++ homework2/task2.py
@@ -7,8 +7,22 @@ import fractions
 n_1, n_2 = map(int, input("Введите строку формата a/b: ").split("/"))
 n_3, n_4 = map(int, input("Введите строку формата a/b: ").split("/"))
 
+numerator = n_1 * n_4 + n_2 * n_3
+denominator = n_2 * n_4
+if denominator % numerator == 0:
+    denominator = denominator // numerator
+    numerator = 1 
+print(f'Сумма дробей {n_1}/{n_2} и {n_3}/{n_4} равна {numerator}/{denominator}')
+
+numerator = n_1 * n_3
+denominator = n_2 * n_4
+if denominator % numerator == 0:
+    denominator = denominator // numerator
+    numerator = 1
+print(f'Произведение дробей {n_1}/{n_2} и {n_3}/{n_4} равно {numerator}/{denominator}')
+
+print('Проверка')
 f1 = fractions.Fraction(n_1, n_2)
 f2 = fractions.Fraction(n_3, n_4)
-
 print(f'Сумма {f1} и {f2} равна {f1 + f2}')
 print(f'Произведение {f1} и {f2} равно {f1 * f2}')
